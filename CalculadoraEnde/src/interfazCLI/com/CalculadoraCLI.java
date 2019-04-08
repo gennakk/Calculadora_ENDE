85fa825ab4ac6b2cf2a979b09f95bce62c7d3c44
Ruben <ruben.sanchez.carrasco@gmail.com>
2019-04-08 19:04:24 +0200

    Javadoc actualizado (param/return)
    Todos los metodos de CalculadoraCLI tienen su Javadoc actualizado con sus parametros y returns documentados.

--- CalculadoraEnde/src/interfazCLI/com/CalculadoraCLI.java
+++ CalculadoraEnde/src/interfazCLI/com/CalculadoraCLI.java
@@ -1,6 +1,12 @@
 package interfazCLI.com;
 
+import java.util.function.BinaryOperator;
+
 public class CalculadoraCLI {
+	
+	
+	
+	
 /**
  * Es el programa principal de linea de comandos
  * nuestra labor será extraer los métodos que haga falta y 
@@ -9,8 +15,11 @@ public class CalculadoraCLI {
  * @param args
  */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LanzarMenu();
+		try {
+			LanzarMenu();
+		} catch (ErrorDivision0 e) {
+			e.getMessage();
+		}
 
 	}
 
@@ -18,21 +27,253 @@ public class CalculadoraCLI {
 	/**
 	 * Muestra un menú de linea de comando con diferentes opciones
 	 * como son meter un operador A, un operador B
-	 * y realziar una operacion sobre ellos
+	 * y realiziar una operacion sobre ellos
 	 */
-private static void LanzarMenu() {
-	System.out.println("Bienvenido a la calculadora");
-	Sumar(); //debes borrarme
+private static void LanzarMenu() throws ErrorDivision0 {
+
+	int a=0;
+	int b=0;
+	int resp;
+	do{
+		System.out.println("Bienvenido a la calculadora");
+		System.out.println("1-SUMAR");
+		System.out.println("2-RESTAR");
+		System.out.println("3-MULTIPLICAR");
+		System.out.println("4-DIVIDIR");
+		System.out.println("5-RAIZ CUADRADA");
+		System.out.println("6-BINARIO");
+		System.out.println("7-VALOR ABSOLUTO");
+		System.out.println("8-LOGARITMO");
+		System.out.println("9-FACTORIAL");
+		System.out.println("10-SALIR ");
+		resp = Consola.leeInt();
+		switch (resp) {
+		case 1:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("Introduce valor para b");
+			b = Consola.leeInt();
+			System.out.println("El resultado es" +  Sumar(a,b));
+			break;
+		case 2:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("Introduce valor para b");
+			b = Consola.leeInt();
+			System.out.println("El resultado es" +  Restar(a,b));
+			break;	
+		
+		case 3:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("Introduce valor para b");
+			b = Consola.leeInt();
+			System.out.println("El resultado es" +  Multiplicar(a,b));
+			break;	
+		
+		case 4:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("Introduce valor para b");
+			b = Consola.leeInt();
+			try {
+				System.out.println("El resultado es" +  Dividir(a,b));
+			}
+			catch (ErrorDivision0 e) {
+				System.err.println(e.getMessage());
+			}
+			break;	
+		
+		case 5:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("El resultado es" +  RaizCuadrada(a));
+			break;	
+			
+		case 6:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("El resultado es" +  Binario(a));
+			break;	
+			
+		case 7:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("El resultado es" +  Absoluto(a));
+			break;	
+			
+			
+		case 8:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("Introduce valor para b");
+			b = Consola.leeInt();
+			System.out.println("El resultado es" +  Logaritmo(a,b));
+			break;	
+			
+		case 9:
+			
+			System.out.println("Introduce valor para a");
+			a = Consola.leeInt();
+			System.out.println("Introduce valor para b");
+			b = Consola.leeInt();
+			System.out.println("El resultado es" +  Factorial(a));
+			break;		
+		default:
+			resp=10;
+			break;
+		}
+	}while(resp!=10);
+}
+/**
+ * Metodo que eleva un n a la m
+ * @author gian piero
+ * @param n numero base
+ * @param m exponente
+ * @return el resultado de exponenciar n a la m
+ */
+public static double Elevar(double n, double m)
+{
+	return Math.pow(n, m);
 }
 
 
-private static int Sumar() {
+/**
+ * Metodo que calcula la raiz cuadrada de n
+ * @author gian piero
+ * @param n numero del que se realizara su raiz cuadrada
+ * @return raiz cuadrada de n
+ */
+public static double RaizCuadrada(double n)
+{
+	try {
+		if (n<0)
+			throw new Exception();
+	}
+	catch (Exception e) {
+		System.out.println("Error no se puede hacer la raiz cuadrada de un numero negativo");
+	}
+	return Math.sqrt(n);
+}
+
+/**
+ * Metodo que calcula el valor absoluto de n
+ * @author gian piero
+ * @param n valor del que se obtendra su valor absoluto
+ * @return valor absoluto de n
+ */
+public static double Absoluto(double n)
+{
+	return Math.abs(n);
+}
+  
+ /**
+ * Realiza la suma entre dos números
+ * @author Rodrigo
+ * @param n numero que se sumara a m
+ * @param m numero que se sumara a n
+ * @return suma de n y m
+ */
+public static double Sumar(double n,double m) {
 	System.out.println("Yo sumo dos operadores");
-	return 0;
+	return n+m;
 
 }
 
+/**
+* Realiza la resta entre dos números
+* @author Rodrigo
+* @param n numero al que se le restara m
+* @param m numero que sera restado a n
+* @return resultado de restarle m a n
+*/
+public static double Restar(double n,double m) {
+	System.out.println("Yo resto dos operadores");
+	return n-m;
+
+}
+
+/**
+* Realiza la multiplicacion entre dos números
+* @author Rodrigo
+* @param n numero que se multiplicara por m
+* @param m numero que se multiplicara por n
+* @return resultado de multiplicar n y m
+*/
+public static double Multiplicar(double n,double m) {
+	System.out.println("Yo multiplico dos operadores");
+	return n*m;
+
+}
+
+/**
+ * Realiza la división entre dos números.
+ * Lanza excepcion cuando el divisor es 0
+ * @author Rodrigo
+ * @param n dividendo
+ * @param m divisor
+ * @return resultado de dividir n entre m
+ */
+public static double Dividir(double n,double m) throws ErrorDivision0{
+	System.out.println("Yo divido dos operadores");
+	
+		if(m==0)
+			throw new ErrorDivision0("Division por 0.");
+		return n/m;	
+
+}
+
+/**
+ * Realiza el logaritmo de n con base m
+ *
+ * @author Rodrigo
+ * @param n numero del logaritmo
+ * @param m base del logaritmo
+ * @return resultado de realizar el logaritmo de n con base m
+ */
+public static double Logaritmo(double n,double m) {
+	
+	System.out.println("Yo hago el logaritmo de dos operadores");
+	
+	return Math.log(n) / Math.log(m);
+	
+}
 
 
+/**
+ * Metodo que calcula el binario absoluto de n
+ * @author gian piero
+ * @param n numero decimal del que se desea saber su binario
+ * @return numero binario correspondiente al numero decimal recibido (n)
+ */
+public static double Binario(double n)
+{
+	return  Double.parseDouble(Integer.toBinaryString((int)n)) ;
+}
+
+
+/**
+ * Metodo que calcula el factorial de n
+ * @author gian piero
+ * @param n numero del que se desea saber su factorial
+ * @return factorial de n
+ */
+public static double Factorial(double n)
+{
+	int i=1;
+	for (int cont=(int) n; cont>0; cont--)
+	{
+		i*=cont;
+	}
+	return i;
+}
 
 }
--- CalculadoraEnde/src/interfazCLI/com/CalculadoraCLITest.java
+++ CalculadoraEnde/src/interfazCLI/com/CalculadoraCLITest.java
@@ -2,7 +2,11 @@ package interfazCLI.com;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * Clase de JUNIT creada para hacer pruebas sobre los métodos que vamos creando
@@ -13,10 +17,275 @@ import org.junit.jupiter.api.Test;
  */
 
 class CalculadoraCLITest {
-
+	
+	/**
+	 * Test número 1 sobre el método suma
+	 * @author Rodrigo
+	 *
+	 */
 	@Test
-	void testMain() {
-		fail("Not yet implemented");
+	void testSumar1() {
+		double solucion = CalculadoraCLI.Sumar(2.0,1.0);
+		
+		assertEquals(3.0, solucion);
 	}
+	
+	
+	/**
+	 * Test número 2 sobre el método suma, fallo
+	 * @author Rodrigo
+	 *
+	 */
+	@Test
+	void testSumar2() {
+		double solucion = CalculadoraCLI.Sumar(3.0,1.0);
+		
+		assertEquals(3.0, solucion);
+	}
+	
+	/**
+	 * Test número 1 sobre el método resta
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testResta1() {
+		double solucion = CalculadoraCLI.Restar(3,4);
+		
+		assertEquals(-1, solucion);
+	}
+	
+	/**
+	 * Test número 1 sobre el método resta, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testResta2() {
+		double solucion = CalculadoraCLI.Restar(3,4);
+		
+		assertNotEquals(-2, solucion);
+	}
+	
+	/**
+	 * Test número 1 sobre el método dividir
+	 * @author Rodrigo
+	 *
+	 */
+	@Test
+	void testDividir1() {
+		double solucion;
+		try {
+			solucion = CalculadoraCLI.Dividir(2.0,1.0);
+			assertEquals(3.0, solucion);
+		} catch (ErrorDivision0 e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
+	/**
+	 * Test número 2 sobre el método dividir, fallo
+	 * @author Rodrigo
+	 *
+	 */
+	@Test
+	void testDividir2() {
+		
+		try {
+			assertEquals("Division por 0.", CalculadoraCLI.Dividir(3.0,0));
+		} catch (ErrorDivision0 e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Test número 1 sobre el método elevar
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testElevar1() {
+		double solucion = CalculadoraCLI.Elevar(2.0,1.0);
+		assertEquals(2.0, solucion);
+	}
+	
+	/**
+	 * Test número 2 sobre el método elevar, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testElevar2() {
+		double solucion = CalculadoraCLI.Elevar(2.0,1.0);
+		assertNotEquals(3.0, solucion);
+	}
+	
+	/**
+	 * Test que realiza pruebas con una serie de parametros
+	 *@author gian piero
+	 */
+	@DisplayName("Prueba con parametros para la multiplicacion")
+	@Test
+	@ParameterizedTest(name = "{index} => a={0}, b={1}, resultado={2}")
+	@CsvSource({"5.0,9.0,45.0","1.0,2.0,2.0","20.0,21.0,420.0", "13.0,18.0,234.0" , "-2.0,-20.0,40.0" , "-3.0,10.0,-30.0"}) 
+	public void testMultiplicarParametros(double a, double b, double resultado)
+	{
+		double solucion = CalculadoraCLI.Multiplicar(a, b);
+		assertEquals(resultado, solucion);
+	}
+	
 
+	/**
+	 * Test número 1 sobre el método multiplicar
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testMultiplicar1() {
+		double solucion = CalculadoraCLI.Multiplicar(2.0,2.0);
+		assertEquals(4.0, solucion);
+	}
+	
+	/**
+	 * Test número 2 sobre el método multiplicar, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testMultiplicar2() {
+		double solucion = CalculadoraCLI.Multiplicar(2.0,1.0);
+		assertNotEquals(4.0, solucion);
+	}
+	
+	
+	
+	/**
+	 * Test número 1 sobre el método raiz cuadrada
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testRaizCuadrada1() {
+		double solucion = CalculadoraCLI.RaizCuadrada(4.0);
+		assertEquals(2.0, solucion);
+	}
+	
+	/**
+	 * Test número 2 sobre el método raiz cuadrada, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testRaizCuadrada2() {
+		double solucion = CalculadoraCLI.RaizCuadrada(4.0);
+		assertNotEquals(1.0, solucion);
+	}
+	
+	
+	
+	/**
+	 * Test número 1 sobre el método binario
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testBinario1() {
+		double solucion = CalculadoraCLI.Binario(2.0);
+		assertEquals(10.0, solucion);
+	}
+	
+	/**
+	 * Test número 1 sobre el método binario, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testBinario2() {
+		double solucion = CalculadoraCLI.Binario(4.0);
+		assertNotEquals(10.0, solucion);
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Test número 1 sobre el método calcular valor absoluto
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testValorAbsoluto1() {
+		double solucion = CalculadoraCLI.Absoluto(-2.0);
+		assertEquals(2.0, solucion);
+	}
+	
+	/**
+	 *  Test número 1 sobre el método calcular valor absoluto, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testValorAbsoluto2() {
+		double solucion = CalculadoraCLI.Absoluto(4.0);
+		assertNotEquals(-4.0, solucion);
+	}
+	
+	
+	
+
+	/**
+	 * Test número 1 sobre el método calcular el logaritmo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testLogaritmo1() {
+		double solucion = CalculadoraCLI.Logaritmo(2.0,8.0);
+		assertEquals(3.0, solucion);
+	}
+	
+	/**
+	 *  Test número 1 sobre el método calcular  el logaritmo, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testLogaritmo2() {
+		double solucion = CalculadoraCLI.Logaritmo(2.0,8.0);
+		assertNotEquals(3.1, solucion);
+	}
+	
+	
+	
+	
+	/**
+	 * Test número 1 sobre el método calcular el factorial
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testFactorial1() {
+		double solucion = CalculadoraCLI.Factorial(4.0);
+		assertEquals(24.0, solucion);
+	}
+	
+	/**
+	 *  Test número 1 sobre el método calcular  el factorial, fallo
+	 * @author gian piero
+	 *
+	 */
+	@Test
+	void testFactorial2() {
+		double solucion = CalculadoraCLI.Factorial(3);
+		assertNotEquals(6.0, solucion);
+	}
+	
+	
+	
 }
